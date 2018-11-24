81b38818c57f96d2665c1f758fbaac358ba81aa2
vaisanem <miikkamv@gmail.com>
2018-11-24 17:04:27 +0200

    KassapaateTest

--- ohtu-viikko4/MatkakorttiMockito/src/main/java/ohtu/maksukortti/Kassapaate.java
+++ ohtu-viikko4/MatkakorttiMockito/src/main/java/ohtu/maksukortti/Kassapaate.java
@@ -9,12 +9,14 @@ public class Kassapaate {
     }
     
     public void lataa(Maksukortti kortti, int summa){
-        kortti.lataa(summa);
+        if (summa > 0) kortti.lataa(summa);
     }
     
     public void ostaLounas(Maksukortti kortti) {
-        kortti.osta(HINTA);
-        myytyjaLounaita++;
+        if (kortti.getSaldo() >= HINTA) {
+            kortti.osta(HINTA);
+            myytyjaLounaita++;
+        }
     }
 
     public int getMyytyjaLounaita() {
--- ohtu-viikko4/MatkakorttiMockito/src/test/java/ohtu/maksukortti/KassapaateTest.java
+++ ohtu-viikko4/MatkakorttiMockito/src/test/java/ohtu/maksukortti/KassapaateTest.java
@@ -24,7 +24,7 @@ public class KassapaateTest {
     }
     
     @Test
-    public void kortiltaVelotetaanHintaJosRahaaOn() {
+    public void kortiltaVeloitetaanHintaJosRahaaOn() {
         when(kortti.getSaldo()).thenReturn(10);
         kassa.ostaLounas(kortti);
         
@@ -33,12 +33,26 @@ public class KassapaateTest {
     }
 
     @Test
-    public void kortiltaEiVelotetaJosRahaEiRiita() {
+    public void kortiltaEiVeloitetaJosRahaEiRiita() {
         when(kortti.getSaldo()).thenReturn(4);
         kassa.ostaLounas(kortti);
         
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
+    }
+    
+    @Test
+    public void positiivinenRahamaaraLadataanKortille() {
+        kassa.lataa(kortti, 1);
+        
+        verify(kortti).lataa(1);
+    }
+    
+    @Test
+    public void negatiivistaRahamaaraaEiLadataKortille() {
+        kassa.lataa(kortti, -1);
+        
+        verify(kortti, times(0)).lataa(anyInt());
     }
       
 }
